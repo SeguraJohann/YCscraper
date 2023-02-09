b467cb81da9979e8d5d952685b31490c5bf1364e
SeguraJohann <johann.segura.chacon@gmail.com>
2023-02-09 13:29:00 -0600

    added spider initial configuration

--- YCscraper/YCscraper/spiders/urlGetter.py
+++ YCscraper/YCscraper/spiders/urlGetter.py
@@ -2,3 +2,9 @@ import scrapy
 from selenium import webdriver
 from scrapy.selector import Selector
 from selenium.webdriver.chrome.options import Options
+
+class MySpider(scrapy.Spider):
+    name = 'urlGetter'
+    start_urls = ['https://www.ycombinator.com/companies?isHiring=true&regions=Fully%20Remote']
+
+    def __init__(self):
