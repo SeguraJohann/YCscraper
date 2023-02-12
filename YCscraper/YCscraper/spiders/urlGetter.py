f6815c450c585ae035dda5ae86250ac7ec6e48e9
SeguraJohann <johann.segura.chacon@gmail.com>
2023-02-12 10:23:00 -0600

    Added selenium configurations

--- YCscraper/YCscraper/spiders/urlGetter.py
+++ YCscraper/YCscraper/spiders/urlGetter.py
@@ -8,3 +8,8 @@ class MySpider(scrapy.Spider):
     start_urls = ['https://www.ycombinator.com/companies?isHiring=true&regions=Fully%20Remote']
 
     def __init__(self):
+        options = Options() 
+        options.headless = True
+        options.page_load_strategy = 'eager'
+        options.add_argument("--headless=new")
+        self.driver = webdriver.Chrome(options=options)
