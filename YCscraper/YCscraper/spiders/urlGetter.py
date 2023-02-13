536db5ebd7fe3c034922327f5c8cda0999e6ba69
SeguraJohann <johann.segura.chacon@gmail.com>
2023-02-13 11:01:00 -0600

    added a scroller

--- YCscraper/YCscraper/spiders/urlGetter.py
+++ YCscraper/YCscraper/spiders/urlGetter.py
@@ -11,15 +11,16 @@ class MySpider(scrapy.Spider):
 
     def __init__(self):
         options = Options() 
-        options.headless = True
+        #options.headless = True
         options.page_load_strategy = 'eager'
-        options.add_argument("--headless=new")
+        #options.add_argument("--headless=new") #to do: not sure if it's necessary since it is already on eager load, I'll check it later
         self.driver = webdriver.Chrome(options=options)
         
     def parse(self, response):
         
         self.driver.get(response.url)
-        time.sleep(5)
-        for i in range(15):
+        time.sleep(5) #to do: wait for the site to load, not sure if it is the most effective way, but as it is a small script I also don't think it worth the effor to figure it out 
+        for i in range(15): #not sure how to make it scroll until there's no more, but as it is a small script I also don't think it worth the effor to figure it out
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             print(i)
             
