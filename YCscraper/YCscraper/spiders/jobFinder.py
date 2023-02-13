444dc37b576ae03f41d3f06a4e9af56c8ddaf8c2
SeguraJohann <johann.segura.chacon@gmail.com>
2023-02-13 13:23:00 -0600

    Added the txt url getter

--- YCscraper/YCscraper/spiders/jobFinder.py
+++ YCscraper/YCscraper/spiders/jobFinder.py
@@ -1,9 +1,15 @@
 import scrapy
 
 class JobSpider(scrapy.Spider):
-    name = "job_spider"
+    name = "jobFinder"
 
     def start_requests(self):
         urls = []
+        with open('urls.txt', 'r') as f:
+            for line in f:
+                urls.append(line.strip())
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+        print(urls)
 
     def parse(self, response):
